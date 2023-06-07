4b1b7ba47928dbbf927ab5e50537e16210b0ccd2
nishu-murmu <nishumurmu017@gmail.com>
2023-06-08 02:17:38 +0530

    refactor: uncommenting code

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,9 +2,9 @@ import { config } from "~config"
 import { getData, parseJwt } from "~utils"
 import { checkCookie } from "~utils"
 
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.tabs.create({ url: config.homePage })
-// })
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.create({ url: config.homePage })
+})
 
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
   if (req.type == 'get_token') {
